4a2ef47ee26e890e28c931b522d8d4339bc4ffa9
kanika monga <kanika.monga@abcplusd.net>
2017-07-25 17:40:43 +0530

    Added new files

---
+++ BasicApp/lib/tests.py
@@ -0,0 +1,2 @@
+def assert_result_is_dictionary(result,type):
+    assert isinstance(result, type) == True
--- BasicApp/lib/util.py
+++ BasicApp/lib/util.py
@@ -4,11 +4,11 @@ class Utils():
     @classmethod
     def todict(self,result):
         ls = []
+        dictresult = {}
         for emp in result:
             d = {}
             for column in emp.__table__.columns:
                 d[column.name] = str(getattr(emp, column.name))
-
             ls.append(d)
             dictresult = {"result" : ls}
         return dictresult
